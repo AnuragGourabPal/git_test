e1476b716427d371b4ca9f77374b1d38ca9c9d30
Kishore Debnath <kishoredbn@engineer.com>
2023-03-19 13:37:43 -0400

    version 3: added if condition inside for-loop to print hello world

--- helloworld.cc
+++ helloworld.cc
@@ -2,7 +2,9 @@
 
 int main() {
     for (int i = 0; i< 10; i++) {
-        std::cout << "hello world!\n";
+        if (i < 5) {
+            std::cout << "hello world!\n";
+        }
     }
     return 0;
 }
